a319d4e25e5b7234c07a5b2cc11e00462c7593f7
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-11-30 17:31:04 -0600

    SImultaneus call vertx consumer microservice

--- src/main/java/io/vertx/book/http/HelloConsumerMicroservice.java
+++ src/main/java/io/vertx/book/http/HelloConsumerMicroservice.java
@@ -1,13 +1,23 @@
 package io.vertx.book.http;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.client.HttpRequest;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.codec.BodyCodec;
+import
+    io.vertx.core.json.JsonObject
+    ;
+import
+    io.vertx.rxjava.core.AbstractVerticle
+    ;
+import
+    io.vertx.rxjava.ext.web.*
+    ;
+import
+    io.vertx.rxjava.ext.web.client.*
+    ;
+import
+    io.vertx.rxjava.ext.web.codec.BodyCodec
+    ;
+import
+    rx.Single
+    ;
 
 public class HelloConsumerMicroservice extends AbstractVerticle {
 
@@ -18,20 +28,26 @@ public class HelloConsumerMicroservice extends AbstractVerticle {
     webClient = WebClient.create(vertx);
     Router router = Router.router(vertx);
     router.get("/").handler(this::invokeMicroservice);
-    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    vertx.createHttpServer().requestHandler(router::accept).listen(8081);
   }
 
   private void invokeMicroservice(RoutingContext routingContext) {
-    HttpRequest<JsonObject> request = webClient
-        .get(8080, "localhost", "/test")
+    HttpRequest<JsonObject> request1 = webClient.get(8080, "localhost", "/luke")
         .as(BodyCodec.jsonObject());
-    request.send(ar -> {
-      if (ar.failed()) {
-        routingContext.fail(ar.cause());
-      } else {
-        routingContext.response().end(ar.result().body().encode());
-      }
-    });
+    HttpRequest<JsonObject> request2 = webClient.get(8080, "localhost", "/leia")
+        .as(BodyCodec.jsonObject());
+    Single<JsonObject> s1 = request1.rxSend().map(HttpResponse::body);
+    Single<JsonObject> s2 = request2.rxSend().map(HttpResponse::body);
+    Single.zip(s1, s2, (luke, leia) -> {
+      //We have the results of both requests in Luke and Leia
+      return new JsonObject()
+          .put("Luke", luke.getString("message"))
+          .put("Leia", leia.getString("message"));
+    }).subscribe(result -> routingContext.response().end(result.encodePrettily()),
+        error -> {
+          error.printStackTrace();
+          routingContext.response().setStatusCode(500).end(error.getMessage());
+        });
   }
 
 }
